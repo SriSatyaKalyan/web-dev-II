62b668d145725f2d11c5b42eaa834f296d5fa9d6
Satya Kalyan <satyakalyan5@gmail.com>
2024-05-10 17:13:17 -0600

    Added Bootstrap to Express

--- templatingDemo/index.js
+++ templatingDemo/index.js
@@ -3,10 +3,13 @@ const app = express();
 const path = require("path");
 
 const redditData = require("./data.json");
-console.log(redditData);
+// console.log(redditData);
+
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+// app.set("public", path.join(__dirname, "/public"));
 
 app.get("/", (req, res) => {
 	res.render("home");
