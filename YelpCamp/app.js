a0eaa9003fc2bee6d68bed923feb7415d8b13331
Satya Kalyan <satyakalyan5@gmail.com>
2024-06-11 15:58:15 -0600

    Adding serverside validations using joi tool

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ExpressError = require("./utils/ExpressError");
+const Joi = require("joi");
 const ejsMate = require("ejs-mate");
 const morgan = require("morgan");
+const catchAsync = require("./utils/catchAsync");
 
 const Campground = require("./models/campground");
 
@@ -34,47 +37,94 @@ app.get("/", (req, res) => {
 	res.render("home");
 });
 
-app.get("/campgrounds", async (req, res) => {
-	const campgrounds = await Campground.find({});
-	res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+	"/campgrounds",
+	catchAsync(async (req, res) => {
+		const campgrounds = await Campground.find({});
+		res.render("campgrounds/index", { campgrounds });
+	})
+);
 
 app.get("/campgrounds/new", (req, res) => {
 	res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
-	const campground = new Campground(req.body.campground);
-	await campground.save();
-	res.redirect(`/campgrounds/${campground._id}`);
+app.post(
+	"/campgrounds",
+	catchAsync(async (req, res) => {
+		// if (!req.body.campground)
+		// 	throw new ExpressError("Invalid Campground Data", 400);
+
+		//server-side validations
+		const campgroundSchema = Joi.object({
+			campground: Joi.object({
+				title: Joi.string().required(),
+				price: Joi.number().required().min(0),
+				image: Joi.string().required(),
+				location: Joi.string().required(),
+				description: Joi.string().required(),
+			}).required(),
+		});
+		const { error } = campgroundSchema.validate(req.body);
+		if (error) {
+			const msg = error.details.map((el) => el.message).join(",");
+			throw new ExpressError(msg, 400);
+		}
+		console.log(result);
+		const campground = new Campground(req.body.campground);
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
+
+app.get(
+	"/campgrounds/:id",
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		res.render("campgrounds/show", { campground });
+	})
+);
+
+app.get(
+	"/campgrounds/:id/edit",
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		res.render("campgrounds/edit", { campground });
+	})
+);
+
+app.put(
+	"/campgrounds/:id",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const campground = await Campground.findByIdAndUpdate(id, {
+			...req.body.campground,
+		});
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
+
+app.delete(
+	"/campgrounds/:id",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		await Campground.findByIdAndDelete(id);
+		res.redirect("/campgrounds");
+	})
+);
+
+// app.use((req, res) => {
+// 	res.status(404).send("NOT FOUND!");
+// });
+
+app.all("*", (req, res, next) => {
+	next(new ExpressError("Page Not Found", "404"));
 });
 
-app.get("/campgrounds/:id", async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
-	res.render("campgrounds/show", { campground });
-});
-
-app.get("/campgrounds/:id/edit", async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
-	res.render("campgrounds/edit", { campground });
-});
-
-app.put("/campgrounds/:id", async (req, res) => {
-	const { id } = req.params;
-	const campground = await Campground.findByIdAndUpdate(id, {
-		...req.body.campground,
-	});
-	res.redirect(`/campgrounds/${campground._id}`);
-});
-
-app.delete("/campgrounds/:id", async (req, res) => {
-	const { id } = req.params;
-	await Campground.findByIdAndDelete(id);
-	res.redirect("/campgrounds");
-});
-
-app.use((req, res) => {
-	res.status(404).send("NOT FOUND!");
+app.use((err, req, res, next) => {
+	const { message = "Something went wrong", statusCode = 500 } = err;
+	if (!err.message) err.message = "Oh no! Something went wrong";
+	res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
